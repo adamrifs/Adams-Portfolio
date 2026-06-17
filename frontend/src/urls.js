a8910af2b197fa0d47fbc2f2571292716bf50c71
Adam <112921206+adamrifs@users.noreply.github.com>
2026-06-17 07:11:21 +0530

    Add backend and admin folders properly, ignore node_modules

--- frontend/src/urls.js
+++ frontend/src/urls.js
@@ -1,2 +1,2 @@
-// export const serverUrl = 'https://adamsportfolio.onrender.com/api/'
-export const serverUrl = 'http://localhost:8000/api/'
+export const serverUrl = 'https://adams-portfolio.onrender.com/api/'
+// export const serverUrl = 'http://localhost:8000/api/'
